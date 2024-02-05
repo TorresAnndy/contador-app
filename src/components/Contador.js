ee544022f9efe358ad129f38f5c4bd263310f939
TorresAnndy <andygonzales2003@gmail.com>
2024-02-05 08:55:56 -0500

    modified:   public/index.html 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	new file:   src/Style/Contador.css 	renamed:    src/index.css -> src/Style/index.css 	new file:   src/components/Contador.js 	modified:   src/index.js

---
+++ src/components/Contador.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import '../Style/Contador.css'
+
+function Contador() {
+    //manejar el estado de una variable/constante
+    const [cont, setcont] = useState(0);
+    //cont almacena el valor
+    //setcont se usa para cambiar el valor
+
+    function handleDisminuir()
+    {
+        setcont(cont -1)
+    }
+
+    function handleAumentar()
+    {
+        setcont(cont +1)
+    }
+    
+    function handleReiniciar()
+    {
+        setcont(0)
+    }
+
+  return (
+    <div className='app'>
+        Contador
+        <div className='box'>
+            <p>{cont}</p>
+            <button onClick={handleDisminuir}>Disminuir</button>
+            <button onClick={handleReiniciar}>Reiniciar</button>
+            <button onClick={handleAumentar}>Aumentar</button>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Contador
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './Style/index.css';
 import reportWebVitals from './reportWebVitals';
+import Contador from './components/Contador';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Contador/>
   </React.StrictMode>
 );
 
